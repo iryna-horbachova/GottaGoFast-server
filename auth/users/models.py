5294ae615e6c9aace42a3cc1cd3203718b28c7db
iryna-horbachova <irachka.gorbacheva@gmail.com>
2022-05-04 10:31:52 +0200

    Add PostgreSQL configuration

--- auth/users/models.py
+++ auth/users/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.base_user import (
+    BaseUserManager, AbstractBaseUser
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+
+    admin = models.BooleanField(default=False)
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
